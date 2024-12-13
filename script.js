dd94bf3afc33e921c25bd488e72292e3d4624aed
Nodyww <aling900423@gmail.com>
2024-12-13 09:43:13 +0800

    '首頁完成版'

---
+++ script.js
@@ -0,0 +1,12 @@
+let image = document.querySelectorAll(".photo img")
+let index = 0;
+
+showNextImage = () =>{
+    image[index].classList.remove("active")
+    index = (index + 1) % image.length
+    image[index].classList.add("active")
+}
+
+setInterval(showNextImage, 5000)
+
+
